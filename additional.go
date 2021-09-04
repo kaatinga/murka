23ccf52504b40f1b61ecff24d075323535d9f0f9
Michael Gunkoff <painter@3lines.club>
2021-09-04 23:48:18 +0300

    additional checkers are implemeted

--- additional.go
+++ additional.go
@@ -1,1 +1,22 @@
 package murka
+
+// CheckUnderscore checks underscore character.
+func CheckUnderscore(value rune) bool {
+	return value == 0x5f
+}
+
+// runAdditionalChecks uses additional functions to check input string.
+func runAdditionalChecks(value rune, additionalCheckers ...func(value rune) bool) bool {
+
+	if len(additionalCheckers) == 0 {
+		return false
+	}
+
+	for _, check := range additionalCheckers {
+		if !check(value) {
+			return false
+		}
+	}
+
+	return true
+}
