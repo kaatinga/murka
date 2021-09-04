6789d8eeedb307f48826898565d94c9e919ae000
Michael Gunkoff <painter@3lines.club>
2021-09-04 23:48:31 +0300

    Validate() implemented

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package murka
+
+// Validate checks characters in the input string. The default charset is a-zA-Z0-9.
+func Validate(pagePath string, additionalCheckers ...func(value rune) bool) error {
+
+	for _, value := range pagePath {
+		if !(value >= 0x61 && value <= 0x7A || // lowercase
+			value >= 0x41 && value <= 0x5A || // uppercase
+			value >= 0x30 && value <= 0x39 || // digits
+			runAdditionalChecks(value, additionalCheckers...)) {
+			return ErrIncorrectCharacter
+		}
+	}
+
+	return nil
+}
