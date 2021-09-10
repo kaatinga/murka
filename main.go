6e269d4e74d624ed0754e2321b28c38dcc0869ac
Michael Gunkoff <painter@3lines.club>
2021-09-10 11:45:57 +0300

    Replace is faster now

--- main.go
+++ main.go
@@ -19,16 +19,14 @@ func Validate(text string, additionalCheckers ...func(value rune) bool) error {
 func Replace(text string, character rune, additionalCheckers ...func(value rune) bool) string {
 
 	// we repeat the size of the new string
-	var newString = make([]rune, len(text))
+	var newString = []rune(text)
 
-	for i, value := range text {
-		if !(value >= 0x61 && value <= 0x7A || // lowercase
-			value >= 0x41 && value <= 0x5A || // uppercase
-			value >= 0x30 && value <= 0x39 || // digits
-			runAdditionalChecks(value, additionalCheckers...)) {
+	for i := 0; i < len(text); i++ {
+		if !(text[i] >= 0x61 && text[i] <= 0x7A || // lowercase
+			text[i] >= 0x41 && text[i] <= 0x5A || // uppercase
+			text[i] >= 0x30 && text[i] <= 0x39 || // digits
+			runAdditionalChecks(rune(text[i]), additionalCheckers...)) {
 			newString[i] = character
-		} else {
-			newString[i] = value
 		}
 	}
 
