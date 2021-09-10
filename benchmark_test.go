7794c50794d651bcfbaac969ca570cee6b6f588c
Michael Gunkoff <painter@3lines.club>
2021-09-10 11:45:28 +0300

    added comparison to strings.ReplaceAll()

--- benchmark_test.go
+++ benchmark_test.go
@@ -1,36 +1,59 @@
 package murka
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
+
+//// nolint
+//func BenchmarkValidate(b *testing.B) {
+//
+//	b.ReportAllocs()
+//	for i := 0; i < b.N; i++ {
+//		Validate("test")
+//		Validate("12345")
+//		Validate("TEST_PAGE", CheckUnderscore)
+//	}
+//}
+//
+//// nolint
+//func BenchmarkValidateByRegexp(b *testing.B) {
+//
+//	b.ReportAllocs()
+//	for i := 0; i < b.N; i++ {
+//		validateByRegexp("test")
+//		validateByRegexp("12345")
+//		validateByRegexp("TEST_PAGE")
+//	}
+//}
+//
+//// nolint
+//func BenchmarkValidateByUnicode(b *testing.B) {
+//
+//	b.ReportAllocs()
+//	for i := 0; i < b.N; i++ {
+//		validateByUnicode("test")
+//		validateByUnicode("12345")
+//		validateByUnicode("TEST_PAGE")
+//	}
+//}
 
 // nolint
-func BenchmarkValidate(b *testing.B) {
+func BenchmarkReplace(b *testing.B) {
 
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		Validate("test")
-		Validate("12345")
-		Validate("TEST_PAGE", CheckUnderscore)
+		Replace("tes:t", '_')
+		Replace("12:3:45", '_')
 	}
 }
 
 // nolint
-func BenchmarkValidateByRegexp(b *testing.B) {
+func BenchmarkStringsReplace(b *testing.B) {
 
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
-		validateByRegexp("test")
-		validateByRegexp("12345")
-		validateByRegexp("TEST_PAGE")
-	}
-}
-
-// nolint
-func BenchmarkValidateByUnicode(b *testing.B) {
-
-	b.ReportAllocs()
-	for i := 0; i < b.N; i++ {
-		validateByUnicode("test")
-		validateByUnicode("12345")
-		validateByUnicode("TEST_PAGE")
+		strings.ReplaceAll("tes:t", ":", "_")
+		strings.ReplaceAll("12:3:45", ":","_")
 	}
 }
